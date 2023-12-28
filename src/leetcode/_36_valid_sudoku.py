2815e7532138570733403f45ebd3741089925531
Nhac Tat Nguyen <nhactat.nguyen_ext@zooplus.com>
2023-12-28 14:34:53 +0100

    LeetCode 36: Valid Sudoku solution 3

--- src/leetcode/_36_valid_sudoku.py
+++ src/leetcode/_36_valid_sudoku.py
@@ -2,20 +2,19 @@ import collections
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = collections.defaultdict(set)
-        cols = collections.defaultdict(set)
-        boxes = collections.defaultdict(set)
+        seen = set()
 
         for r in range(9):
             for c in range(9):
-                current_value = board[r][c]
-                if current_value == ".":
+                curr_val = board[r][c]
+                if curr_val == ".":
                     continue
-                if (current_value in rows[r] or current_value in cols[c] or current_value in boxes[(r // 3, c // 3)]):
+                if ((str(curr_val) + "r" + str(r)) in seen or
+                    (str(curr_val) + "c" + str(c)) in seen or
+                    (str(curr_val) + "b" + str(r//3) + str(c//3)) in seen):
                     return False
-                else:
-                    rows[r].add(current_value)
-                    cols[c].add(current_value)
-                    boxes[(r // 3, c // 3)].add(current_value)
+                seen.add(str(curr_val) + "r" + str(r))
+                seen.add(str(curr_val) + "c" + str(c))
+                seen.add(str(curr_val) + "b" + str(r//3) + str(c//3))
         # Valid
         return True
