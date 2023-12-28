83daf6373c3587cebf9f7c00f47d687490cf3a06
Nhac Tat Nguyen <nhactat.nguyen_ext@zooplus.com>
2023-12-28 14:14:20 +0100

    LeetCode 36: Valid Sudoku solution 2

--- src/leetcode/_36_valid_sudoku.py
+++ src/leetcode/_36_valid_sudoku.py
@@ -1,40 +1,21 @@
+import collections
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # check each row / column
-        for i in range(9):
-            col_set = set()
-            row_set = set()
-            for j in range(9):
-                curr_row_val = board[i][j]
-                if curr_row_val != '.':
-                    if curr_row_val in row_set:
-                        return False
-                    else:
-                        row_set.add(curr_row_val)
-                
-                curr_col_val = board[j][i]
-                if curr_col_val != '.':
-                    if curr_col_val in col_set:
-                        return False
-                    else:
-                        col_set.add(curr_col_val)
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        boxes = collections.defaultdict(set)
 
-        # check each sub-box
-        box_starting_points = [
-            (0, 0), (0, 3), (0, 6),
-            (3, 0), (3, 3), (3, 6),
-            (6, 0), (6, 3), (6, 6)
-        ]
-        for i in range(len(box_starting_points)): # 9 boxes
-            box_set = set()
-            start_row, start_col = box_starting_points[i][0], box_starting_points[i][1]
-            for j in range(3):
-                for k in range(3):
-                    curr_val = board[start_row + j][start_col + k]
-                    if curr_val != '.':
-                        if curr_val in box_set:
-                            return False
-                        else:
-                            box_set.add(curr_val)
+        for r in range(9):
+            for c in range(9):
+                current_value = board[r][c]
+                if current_value == ".":
+                    continue
+                if (current_value in rows[r] or current_value in cols[c] or current_value in boxes[(r // 3, c // 3)]):
+                    return False
+                else:
+                    rows[r].add(current_value)
+                    cols[c].add(current_value)
+                    boxes[(r // 3, c // 3)].add(current_value)
         # Valid
         return True
